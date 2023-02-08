9f38326fa86df4065b18c898e732ea9f9e069575
mobinans <mobin.ans@gmail.com>
2023-02-08 10:47:53 +0530

    fix some bugs

--- app.js
+++ app.js
@@ -45,7 +45,9 @@ app.get("/", (req, res) => {
   res.send("<h1>Jobs API</h1><a href='/api-docs'>Documentation</a>");
 });
 
-app.use("api-docs", swaggerUI.serve, swaggerDocument);
+// app.get("/api-docs", (req, res) => res.send("api"));
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
 app.use("/api/v1/auth", authRouter);
